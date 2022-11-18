d03e5783fb456c6a43c5b184821c59992c58fab3
Zackary Glazewski <zglazewski@umail.ucsb.edu>
2022-11-17 18:00:37 -0800

    ZG - adds test for toast message

--- frontend/src/tests/pages/PersonalSchedules/PersonalSchedulesCreatePage.test.js
+++ frontend/src/tests/pages/PersonalSchedules/PersonalSchedulesCreatePage.test.js
@@ -104,5 +104,57 @@ describe("PersonalSchedulesCreatePage tests", () => {
         expect(mockNavigate).toBeCalledWith({ "to": "/personalschedules/list" });
     });
 
+    test("when you fill in the form and hit submit with a name with length greater than fifteen, the name on the toast is shortened", async () => {
+        const queryClient = new QueryClient();
+        const personalSchedule = {
+            id: 17,
+            name: "SampNameLongerThanFifteenChars",
+            description: "desc",
+            quarter: "W08"
+        };
+
+        axiosMock.onPost("/api/personalschedules/post").reply( 202, personalSchedule );
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <PersonalSchedulesCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByTestId("PersonalScheduleForm-name")).toBeInTheDocument();
+        
+        const nameField = screen.getByTestId("PersonalScheduleForm-name");
+        const descriptionField = screen.getByTestId("PersonalScheduleForm-description");
+        //const quarterField = document.querySelector("#PersonalScheduleForm-quarter");
+        const quarterField = document.querySelector("#PersonalScheduleForm-quarter");
+        //const selectQuarter = getByLabelText("Quarter")
+        const submitButton = screen.getByTestId("PersonalScheduleForm-submit");
+
+        fireEvent.change(nameField, { target: { value: 'SampName' } });
+        fireEvent.change(descriptionField, { target: { value: 'desc' } });
+        fireEvent.change(quarterField, { target: { value: '20124' } });
+        //userEvent.selectOptions(selectQuarter, "20124");
+
+        expect(submitButton).toBeInTheDocument();
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+        expect(quarterField).toHaveValue("20124");
+        //expect(setQuarter).toBeCalledWith("20124"); //need this and axiosMock below?
+
+        expect(axiosMock.history.post[0].params).toEqual(
+            {
+            "name": "SampName",
+            "description": "desc",
+            "quarter": "20124"
+        });
+
+        expect(mockToast).toBeCalledWith("New personalSchedule Created - id: 17 name: SampNameLongerT");
+        expect(mockNavigate).toBeCalledWith({ "to": "/personalschedules/list" });
+    });
 
 });
