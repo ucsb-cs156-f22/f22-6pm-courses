9bdf6bafcdbaed0e48c3be8cc702ba68f7bb48a9
Zackary Glazewski <zglazewski@umail.ucsb.edu>
2022-11-22 18:16:26 -0800

    ZG - adds PersonalSectionsTable and temporary tests

---
+++ frontend/src/fixtures/personalSectionFixtures.js
@@ -0,0 +1,51 @@
+const personalSectionsFixtures = {
+    onePersonalSection: {
+        "courseId": 1,
+        "enrollCd": "12345",
+        "section": "Section",
+        "title": "W22",
+        "enrolled": "11/20",
+        "location": "Broida",
+        "days": "R",
+        "time": "8:00am",
+        "instructor": "Conrad",
+    },
+    twoPersonalSections: [
+        {
+            "courseId": 1,
+            "enrollCd": "12345",
+            "section": "Section",
+            "title": "W22",
+            "enrolled": "11/20",
+            "location": "Broida",
+            "days": "R",
+            "time": "8:00am",
+            "instructor": "Conrad",
+        },
+        {
+            "courseId": 1,
+            "enrollCd": "54321",
+            "section": "Section",
+            "title": "S21",
+            "enrolled": "11/25",
+            "location": "Bren",
+            "days": "W",
+            "time": "8:00am",
+            "instructor": "Conrad",
+        },
+        {
+            "courseId": 1,
+            "enrollCd": "09876",
+            "section": "Section",
+            "title": "S22",
+            "enrolled": "11/30",
+            "location": "Buchanan",
+            "days": "M",
+            "time": "8:00am",
+            "instructor": "Conrad",
+        }
+    ]
+};
+
+
+export { personalSectionsFixtures };
