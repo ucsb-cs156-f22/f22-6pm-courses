aa39bc749d1fa96cf7cdd7e31c1d92dbb44ece66
Saahil Joshi <saahiljoshi@ucsb.edu>
2022-11-22 17:55:49 -0800

    sj - added a storybook entry for new coursesearch page

---
+++ frontend/src/stories/pages/HomePage/CourseSearches/CourseSearchesIndexPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import CourseSearchesIndexPage from "main/pages/CourseSearches/CourseSearchesIndexPage";
+
+export default {
+    title: 'pages/CourseSearches/CourseSearches/CourseSearchesIndexPage',
+    component: CourseSearchesIndexPage
+};
+
+const Template = () => <CourseSearchesIndexPage />;
+
+export const Default = Template.bind({});
+
+
+
+
