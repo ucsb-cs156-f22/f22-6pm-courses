c52ce0c2a2f1a1d51105ddb22d98f7a8bc2bff45
Zackary Glazewski <zglazewski@umail.ucsb.edu>
2022-11-26 20:22:58 -0800

    ZG - adds testing and story for the PersonalSectionsTable component

--- frontend/src/main/components/PersonalSections/PersonalSectionsTable.js
+++ frontend/src/main/components/PersonalSections/PersonalSectionsTable.js
@@ -4,82 +4,52 @@ import { convertToFraction, formatDays, formatInstructors, formatLocation, forma
 
 export default function PersonalSectionsTable({ personalSections }) {
     
+    //personalSections is an array of Course objects
+    
     // Stryker enable all 
 
     const columns = [
         {
             Header: 'Course ID',
             accessor: 'courseId', // accessor is the "key" in the data
-
-            //Cell: ({ cell: { value } }) => value.substring(0, value.length-2) 
         },
         {
             Header: 'Enroll Code',
-            accessor: 'classSections.enrollCode', 
-            disableGroupBy: true,
-
-            aggregate: getFirstVal,
-            Aggregated: ({ cell: { value } }) => `${value}`
+            accessor: 'classSections.enrollCode',
         },
         {
-            // Stryker disable next-line StringLiteral: this column is hidden, very hard to test
             Header: 'Is Section?',
-            accessor: (row) => isSection(row.classSections.section),
-            // Stryker disable next-line StringLiteral: this column is hidden, very hard to test
+            accessor: (row) => String(isSection(row.classSections.section)),
             id: 'isSection',
         },
         {
             Header: 'Title',
             accessor: 'title',
-            disableGroupBy: true,
-
-            aggregate: getFirstVal,
-            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Enrolled',
             accessor: (row) => convertToFraction(row.classSections.enrolledTotal, row.classSections.maxEnroll),
-            disableGroupBy: true,
             id: 'enrolled',
-
-            aggregate: getFirstVal,
-            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Location',
             accessor: (row) => formatLocation(row.classSections.timeLocations),
-            disableGroupBy: true,
             id: 'location',
-
-            aggregate: getFirstVal,
-            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Days',
             accessor: (row) => formatDays(row.classSections.timeLocations),
-            disableGroupBy: true,
             id: 'days',
-
-            aggregate: getFirstVal,
-            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Time',
             accessor: (row) => formatTime(row.classSections.timeLocations),
-            disableGroupBy: true,
             id: 'time',
-
-            aggregate: getFirstVal,
-            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Instructor',
             accessor: (row) => formatInstructors(row.classSections.instructors),
-            disableGroupBy: true,
             id: 'instructor',
-
-            aggregate: getFirstVal,
-            Aggregated: ({ cell: { value } }) => `${value}`
         },
     ];
 
