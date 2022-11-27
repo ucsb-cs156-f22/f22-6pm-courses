d5bfba2b45ce15e0e63cbd18bf13d4cdf3c1ee2e
Zackary Glazewski <zglazewski@umail.ucsb.edu>
2022-11-26 18:32:50 -0800

    ZG - fixes PersonalSectionsTable to align with the input format

--- frontend/src/main/components/PersonalSections/PersonalSectionsTable.js
+++ frontend/src/main/components/PersonalSections/PersonalSectionsTable.js
@@ -1,10 +1,8 @@
 import React from "react";
 import OurTable from "main/components/OurTable";
-import { useBackendMutation } from "main/utils/useBackend";
-import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/PersonalSectionUtils"
-import { useNavigate } from "react-router-dom";
+import { convertToFraction, formatDays, formatInstructors, formatLocation, formatTime, isSection } from "main/utils/sectionUtils.js";
 
-export default function PersonalSectionsTable({ personalSections, showButtons=true }) {
+export default function PersonalSectionsTable({ personalSections }) {
     
     // Stryker enable all 
 
@@ -12,23 +10,35 @@ export default function PersonalSectionsTable({ personalSections, showButtons=tr
         {
             Header: 'Course ID',
             accessor: 'courseId', // accessor is the "key" in the data
+
+            //Cell: ({ cell: { value } }) => value.substring(0, value.length-2) 
         },
-        
         {
             Header: 'Enroll Code',
-            accessor: 'enrollCd',
+            accessor: 'classSections.enrollCode', 
+            disableGroupBy: true,
+
+            aggregate: getFirstVal,
+            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
-            Header: 'Section',
-            accessor: 'section',
+            // Stryker disable next-line StringLiteral: this column is hidden, very hard to test
+            Header: 'Is Section?',
+            accessor: (row) => isSection(row.classSections.section),
+            // Stryker disable next-line StringLiteral: this column is hidden, very hard to test
+            id: 'isSection',
         },
         {
             Header: 'Title',
-            id: 'title',
+            accessor: 'title',
+            disableGroupBy: true,
+
+            aggregate: getFirstVal,
+            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Enrolled',
-            accessor: (row) => convertToFraction(row.section.enrolledTotal, row.section.maxEnroll),
+            accessor: (row) => convertToFraction(row.classSections.enrolledTotal, row.classSections.maxEnroll),
             disableGroupBy: true,
             id: 'enrolled',
 
@@ -37,32 +47,54 @@ export default function PersonalSectionsTable({ personalSections, showButtons=tr
         },
         {
             Header: 'Location',
+            accessor: (row) => formatLocation(row.classSections.timeLocations),
+            disableGroupBy: true,
             id: 'location',
+
+            aggregate: getFirstVal,
+            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Days',
+            accessor: (row) => formatDays(row.classSections.timeLocations),
+            disableGroupBy: true,
             id: 'days',
+
+            aggregate: getFirstVal,
+            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Time',
+            accessor: (row) => formatTime(row.classSections.timeLocations),
+            disableGroupBy: true,
             id: 'time',
+
+            aggregate: getFirstVal,
+            Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Instructor',
+            accessor: (row) => formatInstructors(row.classSections.instructors),
+            disableGroupBy: true,
             id: 'instructor',
+
+            aggregate: getFirstVal,
+            Aggregated: ({ cell: { value } }) => `${value}`
         },
     ];
 
+    const testId = "PersonalSectionsTable";
+
+    // add onto buttonColumns
     const buttonColumns = [
         ...columns,
     ]
 
-    // const columnsToDisplay = showButtons ? buttonColumns : columns;
     const columnsToDisplay = buttonColumns;
 
     return <OurTable
         data={personalSections}
         columns={columnsToDisplay}
-        testid={"PersonalSectionsTable"}
+        testid={testId}
     />;
 };
