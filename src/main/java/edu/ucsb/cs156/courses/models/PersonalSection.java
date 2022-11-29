c7e0b25e35291f50942cf029a740fe605763ad6e
Ethan Nguyen <edn@ucsb.edu>
2022-11-28 23:47:26 -0800

    Added new model Object

---
+++ src/main/java/edu/ucsb/cs156/courses/models/PersonalSection.java
@@ -0,0 +1,25 @@
+package edu.ucsb.cs156.courses.models;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+
+//import org.springframework.security.core.GrantedAuthority;
+import edu.ucsb.cs156.courses.entities.PSCourse;
+import edu.ucsb.cs156.courses.entities.PersonalSchedule;
+import edu.ucsb.cs156.courses.documents.CourseInfo;
+import edu.ucsb.cs156.courses.documents.Section;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PersonalSection {
+    private PSCourse psCourse;
+    private PersonalSchedule personalSchedule;
+    private CourseInfo courseInfo;
+    private Section section;
+    //private Collection<? extends GrantedAuthority> roles;
+}
