075f35addb3ade8b560ae2af85909ac66434825d
Ethan Nguyen <edn@ucsb.edu>
2022-11-30 00:09:26 -0800

    updated model

--- src/main/java/edu/ucsb/cs156/courses/models/PersonalSection.java
+++ src/main/java/edu/ucsb/cs156/courses/models/PersonalSection.java
@@ -4,18 +4,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.AccessLevel;
 
-import org.springframework.security.core.GrantedAuthority;
 import edu.ucsb.cs156.courses.entities.PSCourse;
 import edu.ucsb.cs156.courses.entities.PersonalSchedule;
 import edu.ucsb.cs156.courses.documents.CourseInfo;
 import edu.ucsb.cs156.courses.documents.Section;
-
+ 
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class PersonalSection {
     private PSCourse psCourse;
     private PersonalSchedule personalSchedule;
